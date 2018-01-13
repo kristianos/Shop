b1c386d0bc2147f1fd824cfda0875668febf362d
kristianos <albert_93_albert@yahoo.com>
2018-01-13 11:28:22 +0000

    Added Product filtering

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -3,16 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productcontext,
+            IRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            context = productcontext;
+            productCategories = productCategoryContext;
         }
 
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> categories = this.productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel viewModel = new ProductListViewModel();
+            viewModel.Products = products;
+            viewModel.ProductCategories = categories;
+            return View(viewModel);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
