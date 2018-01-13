e64d1efaa91016370655577af11daff94fac0683
kristianos <albert_93_albert@yahoo.com>
2018-01-13 09:31:40 +0000

    Added Entity Framework support

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
