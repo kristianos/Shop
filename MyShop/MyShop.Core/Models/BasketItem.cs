68e189eef7c1bd5ab377528628f3d82febf63eaf
kristianos <albert_93_albert@yahoo.com>
2018-01-13 18:42:45 +0000

    Added basket

---
+++ MyShop/MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MyShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
